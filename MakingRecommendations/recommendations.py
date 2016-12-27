7edfca70212393c1dfa01fa3a00ae1edc3d3d862
Aravindan <aravindan.venkataraju@gmail.com>
2016-12-27 18:55:49 +0800

    recommendations.py_ver1.2
    added comments and a new function to transform pref data.

--- MakingRecommendations/recommendations.py
+++ MakingRecommendations/recommendations.py
@@ -18,6 +18,17 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
 
+
+''' sim_distance: function that calculates similarity based on Euclidean Distance.
+    parameters: prefs - preference data
+                person1, person2 -  two persons between whom the similarity
+                shoud be determined.
+    Euclidean distance:
+      this is the distance between two people rating the same movies.
+      movies to be on the axes and the ratings of critics are marked on the graph.
+      as euclidean distance approaches 1, the persons are the most similar.
+      possible values are between 0 and 1.      
+'''
 def sim_distance(prefs,person1,person2):
     si={}
     for item in prefs[person1]:
@@ -29,6 +40,17 @@ def sim_distance(prefs,person1,person2):
                        for item in prefs[person1] if item in prefs[person2])
     return 1/(1+sum_of_squares**0.5)
 
+''' sim_pearson: function that calculates similarity based on Pearson Correlation.
+    parameters: prefs - preference data
+                person1, person2 -  two persons between whom the similarity
+                shoud be determined.
+    Pearson Correlation:
+      this is to identify how well a set of data fits in a straight line.
+      this similiarity score also takes into consideration, "grade inflation".
+      one person might consistently give higher rating, while the other does'nt,
+      but the ratings would still fit good in a straight line, meaning that the
+      similarity is close between two critics.
+'''
 def sim_pearson(prefs, person1, person2):
     si=[]
     for item in prefs[person1]:
@@ -61,14 +83,30 @@ def sim_pearson(prefs, person1, person2):
 
     return numerator/denominator
 
+'''topMatches: lists the critics that match the closest to a given critic
+   parameters: prefs - preferences data
+               person - the person to whom the closes matches are to be identified.
+               n - number of matches by ranking to be shown
+               similarity - the function to use to determine similarity.
+'''
 def topMatches(prefs, person, n=5, similarity=sim_pearson):
     scores=[(similarity(prefs, person,other), other)
             for other in prefs if other !=person]
     scores.sort()
     scores.reverse()
     return scores[0:n]
-
-def topRecommendations(prefs, person, similarity=sim_pearson):
+'''getRecommendations: lists the top recommendations from critics to watch a movie.
+   parameters: prefs - preferences data
+               person - person who wants recommendation.
+               similarity - the function to use to determine similarity
+   get the similarty score first.
+   get the weightes total score of the movie that was rated by others, but not by the person.
+   i.e. get sum(product of similarity and the rating for the movie)
+        get the sum of similarity of the critics who rated the movie.
+        divide the former by latter.
+        movie with highest score is to be picked.
+'''
+def getRecommendations(prefs, person, similarity=sim_pearson):
     total={}
     simSums={}
     for others in prefs:
@@ -85,5 +123,14 @@ def topRecommendations(prefs, person, similarity=sim_pearson):
     rankings.sort()
     rankings.reverse()
     return rankings
-
-                        
+'''transformPrefs: tranforms the preference data
+   critic: ((movie:rating)...) is transformed to movie:((critic:rating)..)
+   this will be used to find similar movies.
+'''
+def transformPrefs(pref):
+    movies = {}
+    for critic in pref:
+        for movie in pref[critic]:
+            movies.setdefault(movie, {})
+            movies[movie][critic]=pref[critic][movie]
+    return movies
